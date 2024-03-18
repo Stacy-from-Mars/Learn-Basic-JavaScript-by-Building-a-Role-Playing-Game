20e1af62761b17c93aca241597e4024d1fa1dd41
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-03-18 23:50:08 +0300

    step 148

--- script.js
+++ script.js
@@ -183,8 +183,8 @@ function attack() {
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= getMonsterAttackValue(monsters[fighting].level);
     if (isMonsterHit()) {
+        monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     }
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealth.innerText = monsterHealth;
     if (health <= 0) {
