e20b33467e033d62d60345b2996d8e7a0e6e5e01
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-03-18 23:49:21 +0300

    step 147

--- script.js
+++ script.js
@@ -182,6 +182,8 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= getMonsterAttackValue(monsters[fighting].level);
+    if (isMonsterHit()) {
+    }
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealth.innerText = monsterHealth;
